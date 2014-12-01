a05d5136db0c8ff5c9a998c832de32758588d1cc
Michael Dudley <michael.dudley@improvingenterprises.com>
2014-11-30 20:26:20 -0600

    Hello World Demo Working

---
+++ sourceCode/HelloWorld/HelloWorld/app/controllers/helloController.js
@@ -0,0 +1,16 @@
+﻿angular
+    .module('helloApp')
+    .controller('helloController', ['$scope', 'messagesModel', function ($scope, messagesModel) {
+        $scope.firstName = '';
+        $scope.lastName = '';
+        $scope.fullName = function () {
+            return $scope.firstName + ' ' + $scope.lastName;
+        }
+        $scope.showMessage = function () {
+            return $scope.firstName || $scope.lastName;
+        }
+        $scope.message = messagesModel.randomMessage();
+        $scope.newMessage = function () {
+            $scope.message = messagesModel.randomMessage();
+        };
+    }]);
---
+++ sourceCode/HelloWorld/HelloWorld/app/models/messagesModel.js
@@ -0,0 +1,10 @@
+﻿angular
+    .module('helloApp')
+    .factory('messagesModel', function () {
+        var messages = ['Hello', 'Hi', 'Howdy'];
+        return {
+            randomMessage: function() {
+                return messages[_.random(2)];
+            }
+        }
+    })
